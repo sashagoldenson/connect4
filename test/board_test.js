91fa79389f5a7a469ed9f411df28808cf3ca8618
Sasha Goldenson <sgoldens@gmail.com>
2018-03-16 16:38:37 -0700

    Black player win/fail state tests

--- test/board_test.js
+++ test/board_test.js
@@ -44,15 +44,6 @@ describe("board", function() {
     board1.moveCounter = 0;
   });
 
-  afterEach( function() {
-    board1.cells = [[0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0]]
-  });
-
   describe("Red win states", function() {
     it("can win horizontally", function() {
     board1.cells = [[0,0,0,0,0,0,0],
@@ -151,6 +142,110 @@ describe("board", function() {
     [0,1,1,2,2,0,0],
     [1,2,2,1,2,1,1],
     [1,1,2,2,1,1,2]]
+
+    board1.checkForWin(3,2);
+    expect(board1.gameOver).to.be.false
+    });
+  })
+
+  describe("Black win states", function() {
+    it("can win horizontally", function() {
+    board1.cells = [[0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [1,0,0,0,0,0,0],
+    [1,0,1,2,2,2,2],
+    [2,2,1,1,2,1,1]]
+
+    board1.checkForWin(4,3);
+    expect(board1.gameOver).to.be.true
+    });
+
+    it("can win vertically", function() {
+    board1.cells = [[0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [2,0,0,0,0,0,0],
+    [2,1,0,0,0,0,0],
+    [2,1,0,0,0,0,0],
+    [2,1,1,0,0,0,0]]
+
+    board1.checkForWin(2,0);
+    expect(board1.gameOver).to.be.true
+    });
+
+    it("can win diagonally NE to SW", function() {
+    board1.cells = [[0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,2,0],
+    [0,0,0,0,2,1,0],
+    [0,0,0,2,1,1,0],
+    [0,0,2,1,1,1,0]]
+
+    board1.checkForWin(2,6);
+    expect(board1.gameOver).to.be.true
+    });
+
+    it("can win diagonally NW to SE", function() {
+    board1.cells = [[0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,2,0,0,0,0,0],
+    [1,1,2,1,1,0,0],
+    [2,1,1,2,2,2,2],
+    [2,2,1,1,2,2,1]]
+
+    board1.checkForWin(2,1);
+    expect(board1.gameOver).to.be.true
+    });
+  });
+
+  describe("Black fail states", function () {
+
+    it("cannot win horizontally with less than four in a row", function() {
+    board1.cells = [[0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,1,1,0,0,0],
+    [1,0,2,2,2,0,0]]
+
+    board1.checkForWin(5,4);
+    expect(board1.gameOver).to.be.false
+    });
+
+
+    it("cannot win vertically with less than four in a row", function() {
+    board1.cells = [[0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,2,0,0,0],
+    [0,0,0,2,1,0,0],
+    [0,0,0,2,1,1,0]]
+
+    board1.checkForWin(3,3);
+    expect(board1.gameOver).to.be.false
+    });
+
+
+    it("cannot win diagonally NE to SW with less than four in a row", function() {
+    board1.cells = [[0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,2,0,0],
+    [0,0,1,2,2,0,0],
+    [0,0,2,1,1,0,1]]
+
+    board1.checkForWin(3,4);
+    expect(board1.gameOver).to.be.false
+    });
+
+
+    it("cannot win diagonally NW to SE with less than four in a row", function() {
+    board1.cells = [[0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,1,0,0,0,0,0],
+    [0,2,2,1,1,0,0],
+    [2,1,1,2,1,2,2],
+    [2,2,1,1,2,2,1]]
 
     board1.checkForWin(3,2);
     expect(board1.gameOver).to.be.false
